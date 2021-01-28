71d26fa037b04c7b200be7b570c60d20152d2cfc
hridoy <hridoyswe@gmail.com>
2021-01-28 12:51:22 +0600

    EmployeeMangement

---
+++ EmployeeManagement/EmployeeManagement/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using EmployeeManagement.Models;
+using EmployeeManagement.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace EmployeeManagement.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        
+        public ViewResult Index()
+        {
+            var model = _employeeRepository.GetAllEmployees();
+            return View(model);
+        }
+
+        
+        public ViewResult Details(int? id)
+        {
+            HomeDetailsViewModel homeDetailsViewModel = new HomeDetailsViewModel()
+            {
+                Employee = _employeeRepository.GetEmployee(id ?? 1),
+                PageTitle = "Employee Details"
+            };
+            //Employee model = _employeeRepository.GetEmployee(1);
+            //ViewBag.PageTitle = "Employee Details";
+
+            return View(homeDetailsViewModel);
+
+            //MyViews/Test.cshtml
+
+            /* ViewData Example
+               ViewData["Employee"] = model;
+               ViewData["PageTitle"] = "Employee Details";
+            */
+
+            /* 
+                ViewBag Example
+                ViewBag.Employee = model;
+                ViewBag.PageTitle = "Employee Details";
+            */
+        }
+        [HttpGet]
+        public ViewResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public RedirectToActionResult Create(Employee employee)
+        {
+           Employee newEmployee =  _employeeRepository.Add(employee);
+           return RedirectToAction("details", new { id = newEmployee.Id });
+        }
+    }
+}
---
+++ EmployeeManagement/EmployeeManagement/Models/MockEmployeeRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employeeList;
+
+
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee() { Id = 1, Name = "Hridoy", Department = Dept.HR, Email = "hridoy@gmail.com"},
+                new Employee() { Id = 2, Name = "Hasan", Department = Dept.IT, Email = "hasan@gmail.com"},
+                new Employee() { Id = 3, Name = "Mahmudul", Department = Dept.IT, Email = "mahmudul@gmail.com"}
+            };
+        }
+
+        public Employee Add(Employee employee)
+        {
+            employee.Id =  _employeeList.Max(e => e.Id) + 1;
+            _employeeList.Add(employee);
+            return employee;
+        }
+
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            return _employeeList;
+        }
+
+        public Employee GetEmployee(int id)
+        {
+            return _employeeList.FirstOrDefault(e => e.Id == id);
+        }
+    }
+}
