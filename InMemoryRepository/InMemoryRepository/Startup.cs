ba4978a800e359af3142023792465751f999e6a9
hridoy <hridoyswe@gmail.com>
2021-02-03 01:01:41 +0600

    MemoryRepository. There have two part. Number One is InMemoryRepository and another one SQLRepository.

---
+++ InMemoryRepository/InMemoryRepository/Startup.cs
@@ -0,0 +1,52 @@
+using InMemoryRepository.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InMemoryRepository
+{
+    public class Startup
+    {
+        private readonly IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+           _config  = config;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().AddXmlDataContractSerializerFormatters();
+            //Use In Memory Repository
+            services.AddScoped<IEmployeeRepository, MockEmployeeRepository>();
+        }//SQLEmployeeRepository use this..then it is SQL Repository
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+    }
+}
